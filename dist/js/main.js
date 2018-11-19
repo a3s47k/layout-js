ee383282575e80b07a78b34dfdcfeaffe4b5ebdb
Bao Nguyen <baonguyenyam@gmail.com>
2018-11-19 13:30:26 +0700

    addons

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,7 +31,27 @@ $(function () {
         $('select.form-control').removeClass('form-control').css('width', '100%');
     }
 });
-$('.boxinput input').on('focus blur', function () {
-    $(this).parents('.boxinput').toggleClass('active');
+$('.boxinput input').on('focus', function () {
+    $(this).parents('.boxinput').addClass('active');
 });
+$('.boxinput input').on('blur', function () {
+    $(this).val() ? $(this).parents('.boxinput').addClass('active') : $(this).parents('.boxinput').removeClass('active');
+});
+
+// function getUrlParameter(param, dummyPath) {
+// 	var sPageURL = dummyPath || window.location.search.substring(1),
+// 		sURLVariables = sPageURL.replace(/%2C/g, ",").split(/[&||?]/),
+// 		res;
+// 	for (var i = 0; i < sURLVariables.length; i += 1) {
+// 		var paramName = sURLVariables[i],
+// 			sParameterName = (paramName || "").split("=");
+
+// 		if (sParameterName[0] === param) {
+// 			res = sParameterName[1];
+// 		}
+// 	}
+// 	return res;
+// }
+
+// console.log(getUrlParameter("step"))
 //# sourceMappingURL=main.js.map
