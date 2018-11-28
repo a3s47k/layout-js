b1e2ee7655b3f6ca44b73cc6af28376aaaadfe9d
Bao Nguyen <baonguyenyam@gmail.com>
2018-11-28 14:46:23 +0700

    cmt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,29 @@
-
 $(document).ready(function () {
 	$('.shop [data-toggle="modal"]').each(function () {
-		$(this).click(function(){
+		$(this).click(function () {
+
+
 			var getcnt = $(this).parents('.cnt')
+
 			$('#quickView .modal-title h2').html(getcnt.find('h2').html())
 			$('#quickView .modal-body .col-7').html(getcnt.find('p').html())
 			$('#quickView .modal-body .col-5').html(getcnt.find('.hehe').html())
 
-			$('#quickView .owl-carousel').removeClass('d-none').owlCarousel({
-				loop:true,
-				nav:true,
-				item: 1
+			$('#quickView').on('show.bs.modal', function (e) {
+				$('#quickView .owl-carousel').owlCarousel({
+					loop: true,
+					nav: true,
+					item: 1
+				})
 			})
+			setTimeout(() => {
+				$('#quickView .d-none').removeClass('d-none')
+			}, 700);
+
+
+
+
 
 		})
 	})
 });
-
