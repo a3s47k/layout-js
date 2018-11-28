6dbe63eb185fe0c9f09be11771536ad9b301fac3
Bao Nguyen <baonguyenyam@gmail.com>
2018-11-28 16:10:20 +0700

    dsgfdg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,15 +6,22 @@ var appSEO = {
 		lg: '10.827831',
 		ln: '106.681231',
 		code: 'VN'
+	},
+	buildForm: function(a,b) {
+		return '<div class="input-group mb-3"> <div class="input-group-prepend"><span class="input-group-text">' + a.charAt(0).toUpperCase() + a.slice(1) + '</span></div> <input class="form-control" id="seo-' + a + '" type="text" placeholder="' + b + '"> </div>'
 	}
 }
 for (var key in appSEO) {
 	if (appSEO.hasOwnProperty(key)) {
-		let inputTmp = '<div class="input-group mb-3"> <div class="input-group-prepend"><span class="input-group-text">' + key.charAt(0).toUpperCase() + key.slice(1) + '</span></div> <input class="form-control" id="seo-' + key + '" type="text" placeholder="' + appSEO[key] + '"> </div>'
 		// Xét ĐK
-		if (typeof appSEO[key] !== 'object') {
-			$('#genform').append(inputTmp)
+		if (typeof appSEO[key] === 'string') {
+			$('#genform').append(appSEO.buildForm(key,appSEO[key]))
+		} else if(typeof appSEO[key] === 'object') {
+			for (var k in appSEO[key]) {
+				if (appSEO[key].hasOwnProperty(k)) {
+					$('#genform').append(appSEO.buildForm(k,appSEO[key][k]))
+				}
+			}
 		}
-
 	}
 }
